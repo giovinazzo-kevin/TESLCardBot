0480764c383d93a68a47eb0f0aadb495175e1682
Kappa G3 <Kappa G3>
2016-08-10 19:23:38 +0200

    Improved bot's parsing slightly and formatted its replies better.

--- rtesl.py
+++ rtesl.py
@@ -16,16 +16,18 @@ def find_card_mentions(s):
 def build_response(cards):
     response = 'Here are the cards you mentioned: \n\n'
     for card in cards:
-        card_name = re.sub(r'[\s_\-"\']', '', card).lower()
+        card_name = re.sub(r'[\s_\-"\',;]', '', card).lower()
         url = CARD_DATABASE_URL.format(card_name)
         # Check if the given card is a valid card
         r = requests.get(url)
         if r.headers['content-type'] == 'image/png':
             response += '- [{}]({})\n\n'.format(card, url)
         else:
-            response += '{}: This card does not seem to exist. Possible typo?'.format(card)
-    response += '\n\n_I am a bot, and this action was performed automatically. ' \
-                'For info or to submit a bug report, please contact /u/G3Kappa._'
+            response += '- {}: This card does not seem to exist. Possible typo?\n\n'.format(card)
+    response += '&nbsp;\n\n___\n^(_I am a bot, and this action was performed automatically. ' \
+                'For information or to submit a bug report, please contact /u/​G3Kappa._)'
+    response += '\n\n[Source Code](https://github.com/G3Kappa/TESLCardBot/) ' \
+                '| [Send PM](https://www.reddit.com/message/compose/?to=G3Kappa)'
     return response
 
 
@@ -40,8 +42,8 @@ if __name__ == '__main__':
     r.login(username=os.environ['REDDIT_USERNAME'], password=os.environ['REDDIT_PASSWORD'], disable_warning=True)
     print('TESLCardBot started!')
 
-    streams = itertools.chain(praw.helpers.comment_stream(r, 'elderscrollslegends'), \
-              praw.helpers.submission_stream(r, 'elderscrollslegends'))
+    streams = itertools.chain(praw.helpers.comment_stream(r, 'fieropa'),
+              praw.helpers.submission_stream(r, 'fieropa'))
 
     for s in streams:
         cards = find_card_mentions(s.body)
@@ -50,5 +52,5 @@ if __name__ == '__main__':
                 reply_to(s, cards)
                 s.save() # Exploiting Reddit's servers has never been this easy!
             except:
-                print('There was an error while trying to reply to {}'.format(s))
+                print('There was an error while trying to reply to: {}.'.format(s.id))
             print('Done replying and saved post. ({})'.format(s.id))
