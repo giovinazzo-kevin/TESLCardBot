15b26ff7ffdd223139fa21323d2aa231df59fc70
Kappa G3 <Kappa G3>
2016-08-10 19:24:56 +0200

    aAAH WRONG SUB IN PRODUCTION ABORT ABORT

--- rtesl.py
+++ rtesl.py
@@ -42,8 +42,8 @@ if __name__ == '__main__':
     r.login(username=os.environ['REDDIT_USERNAME'], password=os.environ['REDDIT_PASSWORD'], disable_warning=True)
     print('TESLCardBot started!')
 
-    streams = itertools.chain(praw.helpers.comment_stream(r, 'fieropa'),
-              praw.helpers.submission_stream(r, 'fieropa'))
+    streams = itertools.chain(praw.helpers.comment_stream(r, 'elderscrollslegends'),
+              praw.helpers.submission_stream(r, 'elderscrollslegends'))
 
     for s in streams:
         cards = find_card_mentions(s.body)
