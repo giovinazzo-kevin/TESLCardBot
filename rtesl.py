60ffa583b002e40c9d6eb03b325cfcf09df517db
Kappa G3 <Kappa G3>
2016-08-11 05:53:20 +0200

    Still refactoring

--- rtesl.py
+++ rtesl.py
@@ -1,5 +1,5 @@
 import requests
-import thread
+import threading
 import praw
 import re
 import os
@@ -89,8 +89,14 @@ if __name__ == '__main__':
     r.login(username=os.environ['REDDIT_USERNAME'], password=os.environ['REDDIT_PASSWORD'], disable_warning=True)
     print('TESLCardBot started! ({} MODE)'.format('PRODUCTION' if not TEST_MODE else 'DEVELOPMENT'))
 
-    thread.start_new_thread(monitor_submissions, ())
-    thread.start_new_thread(monitor_comments, ())
+    submissions_thread = threading.Thread(target=monitor_submissions)
+    comments_thread = threading.Thread(target=monitor_comments)
+
+    submissions_thread.start()
+    comments_thread.start()
+
+    submissions_thread.join()
+    comments_thread.join()
 
 
 
